475633a2be87b0f3c2ddecd24e83e1aa7e842554
Ana2k <anushkachakra@gmail.com>
2021-09-21 20:05:42 +0530

    item file layout changes

---
+++ JsonExtractorFirebase/app/src/main/java/com/example/androidjsonextractor/UsersApiService.kt
@@ -0,0 +1,23 @@
+package com.example.androidjsonextractor
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+class UsersApiService{
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(JacksonConverterFactory.create())
+        .build()
+
+    interface UsersApiService{
+        @GET("users")
+        //this adds to back of BASE_URL
+        suspend fun getAllUsers(): Call<List<UsersProperty>>
+    }
+
+
+
+}
