7f800586005bac2eb2a8dffaf04feabc124d4499
Ana2k <anushkachakra@gmail.com>
2021-09-27 11:25:30 +0530

    before implementing adadpter with only viewHolder

--- JsonExtractorFirebase/app/src/main/java/com/example/androidjsonextractor/UsersAdapter.kt
+++ JsonExtractorFirebase/app/src/main/java/com/example/androidjsonextractor/UsersAdapter.kt
@@ -2,15 +2,14 @@ package com.example.androidjsonextractor
 
 import android.content.Context
 import android.view.ViewGroup
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
 class UsersAdapter(val context: Context,): RecyclerView.Adapter {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsersViewHolder {
         TODO("Not yet implemented")
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    fun onBindViewHolder(holder: UsersViewHolder, position: Int) {
         TODO("Not yet implemented")
     }
 }
